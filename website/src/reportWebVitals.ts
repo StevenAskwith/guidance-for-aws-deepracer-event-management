7a3a57efb6b4dc14ca16de312791409f8276a4c9
Steven Askwith <askwith@amazon.co.uk>
2026-02-18 09:58:08 +0000

    Version upgrade and dependancies

--- website/src/reportWebVitals.ts
+++ website/src/reportWebVitals.ts
@@ -1,15 +1,15 @@
-import { ReportHandler } from 'web-vitals';
+import type { MetricType } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler): void => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void): void => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        import('web-vitals').then(({ onCLS, onFCP, onLCP, onTTFB, onINP }) => {
+            onCLS(onPerfEntry);
+            onFCP(onPerfEntry);
+            onLCP(onPerfEntry);
+            onTTFB(onPerfEntry);
+            onINP(onPerfEntry);
+        });
+    }
 };
 
 export default reportWebVitals;
