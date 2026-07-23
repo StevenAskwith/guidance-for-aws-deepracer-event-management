47c5ffe09aa15f753dc7619aeed6ed12d33716a9
Lars Ludvigsen <larsll@outlook.com>
2026-07-23 19:14:18 +0200

    Next attempt

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -32,6 +32,7 @@ class InfrastructurePipelineStage extends Stage {
   public readonly sourceBucketName: cdk.CfnOutput;
   public readonly dremWebsiteUrl: cdk.CfnOutput;
   public readonly appsyncId: cdk.CfnOutput;
+  public readonly uploadBucketName: cdk.CfnOutput;
 
   constructor(scope: Construct, id: string, props: InfrastructurePipelineStageProps) {
     super(scope, id, props);
@@ -55,6 +56,7 @@ class InfrastructurePipelineStage extends Stage {
     this.sourceBucketName = stack.sourceBucketName;
     this.dremWebsiteUrl = stack.dremWebsiteUrl;
     this.appsyncId = stack.appsyncId;
+    this.uploadBucketName = stack.uploadBucketName;
   }
 }
 export interface CdkPipelineStackProps extends cdk.StackProps {
@@ -275,6 +277,31 @@ export class CdkPipelineStack extends cdk.Stack {
       rolePolicyStatements: rolePolicyStatementsForWebsiteDeployStages,
     });
     infrastructure_stage.addPost(websiteDeployStep);
+
+    // Upload default models directly from the source checkout — avoids bundling
+    // 249 MB of .tar.gz files into cdk.out (which would exceed the CodePipeline
+    // artifact size limit for SynthAndDeployBackend_Output).
+    const defaultModelsDeployStep = new pipelines.CodeBuildStep('DeployDefaultModels', {
+      buildEnvironment: {
+        computeType: codebuild.ComputeType.SMALL,
+      },
+      commands: [
+        'aws s3 sync ./lib/default_models/' +
+          ' s3://$uploadBucketName/private/$AWS_DEFAULT_REGION:00000000-0000-0000-0000-000000000000/000000000000/default/' +
+          ' --no-progress',
+      ],
+      envFromCfnOutputs: {
+        uploadBucketName: infrastructure.uploadBucketName,
+      },
+      rolePolicyStatements: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: ['s3:PutObject', 's3:ListBucket', 's3:DeleteObject'],
+          resources: ['*'],
+        }),
+      ],
+    });
+    infrastructure_stage.addPost(defaultModelsDeployStep);
 
     // Post-deploy tests — run after MainSiteDeployToS3 completes
     const postDeployStep = new pipelines.CodeBuildStep('PostDeployTests', {
